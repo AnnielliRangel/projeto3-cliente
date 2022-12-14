94e6c73218a286000ee7483ddce36d6e9b18cd78
sergiofrancalino <sergio.francalino@tjac.jus.br>
2022-12-14 16:43:05 -0300

    ServicesPage.js cores padrao

--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -66,99 +66,137 @@ function ServicesPage() {
     <Container className="container-principal" fluid>
       <Row>
         <Col sm={2}>
-          <NavBar/>
+          <NavBar />
         </Col>
         <Col sm={10}>
-          <Container>
-            <Form>
-        
-           <Form.Group className="mt-3">
-            <Form.Label>Nome do Serviço</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Insira o nome do serviço público"
-              name="details"
-              value={form.details}
-              onChange={handleChange}
-              autoFocus
-            />
-    
-           </Form.Group>
-           <Form.Group className="mt-3">
-            <Form.Label>Validade / Disponibilidade do Serviço</Form.Label>
-            <Form.Control
-              type="date"
-              name="dateFin"
-              value={form.dateFin}
-              onChange={handleChange}
-            />
-          
-           </Form.Group>
-           <Form.Group className="mt-3">
-            <Form.Label>Unidade Prestadora</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Insira o nome do setor / local "
-              name="localSetor"
-              value={form.localSetor}
-              onChange={handleChange}
-            />           
-           </Form.Group>
-
-           <Button variant="primary" className="m-3" onClick={handleSubmit}>
-            Salvar
-           </Button>
-           <Link to={"/tabela"}>
-            <Button variant="secondary" type="submit">
-              Cancelar
-            </Button>
-           </Link>      
-           </Form>
-          </Container>  
-
-      <Container className="border rounded mt-3">
-        <h3 className="mt-3">Gestao dos Serviços</h3>
-        {services.map((service) => {
-          return (
-            <Card key={service._id} className="m-4">
+          <Row>
+            <Card className="card-form">
+              <Card.Header>FORMULÁRIO DE CADASTRO DE SERVIÇO</Card.Header>
               <Card.Body>
-                <h4>{service.details} → Local: {service.localSetor}</h4>
-              </Card.Body>
-              <Card.Footer className="d-flex justify-content-around">
-                {service.discontinued ? (
-                  <p>
-                    {" "}
+                <Card.Text>
+                  <Form>
+                    <Form.Group className="mt-3">
+                      <Form.Label>Nome do Serviço Público</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Insira o nome do serviço prestado..."
+                        name="details"
+                        value={form.details}
+                        onChange={handleChange}
+                        autoFocus
+                      />
+                    </Form.Group>
+                    <Form.Group className="mt-3">
+                      <Form.Label>
+                        Vigência → Disponibilidade do Serviço
+                      </Form.Label>
+                      <Form.Control
+                        type="date"
+                        name="dateFin"
+                        value={form.dateFin}
+                        onChange={handleChange}
+                      />
+                    </Form.Group>
+                    <Form.Group className="mt-3">
+                      <Form.Label>Unidade Prestadora</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Insira o nome do setor ou local... "
+                        name="localSetor"
+                        value={form.localSetor}
+                        onChange={handleChange}
+                      />
+                    </Form.Group>
+
                     <Button
-                      variant="danger"
-                      size="sm"
-                      onClick={(e) => handleDeleteService(e, service._id)}
+                      variant="primary"
+                      className="m-3"
+                      onClick={handleSubmit}
                     >
-                      Excluir Serviço
+                      Salvar
                     </Button>
-                    → Serviço Descontinuado em:{" "}
-                    {service.dateFin.slice(0, 10)}
-                  </p>
-                ) : (
-                  <p>
-                    <Button                      
-                      variant="success"
-                      size="sm"
-                      onClick={(e) => handleServiceDiscontinued(e, service._id)}
-                    >
-                    Cessar Serviço
-                    </Button>
-                    → Serviço Disponível até:{" "}
-                    {service.dateFin.slice(0, 10)}
-                  </p>
-                )}
-              </Card.Footer>
+                    <Link to={"/tabela"}>
+                      <Button variant="secondary" type="submit">
+                        Cancelar
+                      </Button>
+                    </Link>
+                  </Form>
+                </Card.Text>
+              </Card.Body>
             </Card>
-          );
-        })}
+          </Row>
+          <Row>
+            <Card className="card-form">
+              <Card.Header>FORMULÁRIO DE GESTÃO DOS SERVIÇOS</Card.Header>
+              <Card.Body>
+                <Card.Text>
+                  <Form>
+                    <Row className="justify-content-md-center">
+                      <Col>
+                        <Form.Group className="mb-3">
+                          <Form.Label>
+                            {" "}
+                            Serviço ⟷ Unidade Administrativa - UA
+                          </Form.Label>
+
+                          {services.map((service) => {
+                            return (
+                              <Card key={service._id} className="m-4">
+                                <Card.Body>
+                                  <h4>{`|→  ${service.details} | (UA): ${service.localSetor}`}</h4>
+                                </Card.Body>
+                                <Card.Footer className="d-flex justify-content-around">
+                                  {service.discontinued ? (
+                                    <p>
+                                      <Button
+                                        variant="danger"
+                                        size="sm"
+                                        onClick={(e) =>
+                                          handleDeleteService(e, service._id)
+                                        }
+                                      >
+                                        Excluir Serviço
+                                      </Button>
+                                      {`→ Serviço Descontinuado em: ${service.dateFin.slice(
+                                        0,
+                                        10
+                                      )}`}
+                                    </p>
+                                  ) : (
+                                    <p>
+                                      <Button
+                                        variant="success"
+                                        size="sm"
+                                        onClick={(e) =>
+                                          handleServiceDiscontinued(
+                                            e,
+                                            service._id
+                                          )
+                                        }
+                                      >
+                                        Cessar Serviço
+                                      </Button>
+                                      {`→ Serviço Disponível até: ${service.dateFin.slice(
+                                        0,
+                                        10
+                                      )}`}
+                                    </p>
+                                  )}
+                                </Card.Footer>
+                              </Card>
+                            );
+                          })}
+                        </Form.Group>
+                      </Col>
+                    </Row>
+                  </Form>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Row>
+        </Col>
+      </Row>
     </Container>
-   </Col>
-   </Row>
-   </Container>
   );
 }
 
